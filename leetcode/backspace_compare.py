c0c5ba99d0310e63a9dddf1791b51282096210d6
Pedram T'Kanchi <cs.wizardry@gmail.com>
2020-11-15 20:23:40 -0800

    added backspace compare problem

---
+++ leetcode/backspace_compare.py
@@ -0,0 +1,19 @@
+def evaluate(S):
+    s = []
+
+    for ch in S:
+        if ch == "#" and len(s) > 0:
+            s.pop()
+        elif ch != "#":
+            s.append(ch)
+
+    print(s)
+
+    return "".join(s)
+
+def backspace_compare(S, T):
+    return evaluate(S) == evaluate(T)
+
+S = "y#fo##f"
+T = "y#f#o##f"
+print(backspace_compare(S, T))
